afe45b7ee6231ded16aef7160b2019c30402e1e5
shikao-web <sikao1st30@gmail.com>
2026-04-26 21:06:16 +0900

    modified:   components/layout/site-data.ts

--- components/layout/site-data.ts
+++ components/layout/site-data.ts
@@ -1,6 +1,6 @@
 export const navLinks = [
   { href: '/', label: 'Home', external: false },
-  { href: '/services', label: '活動内容紹介', external: false },
+  { href: '/service', label: '活動内容紹介', external: false },
   { href: '/news', label: 'News', external: false },
   { href: 'https://github.com/shikao-web', label: 'GitHub', external: true },
   { href: 'https://zenn.dev/shikaoweb', label: 'Zenn', external: true },
