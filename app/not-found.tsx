aa90e5a8f1b8ae317031280651824b37c56ca871
shikao-web <sikao1st30@gmail.com>
2026-04-26 21:44:45 +0900

    new file:   app/not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+
+export default async function HomePage() {
+
+  return (
+    <>
+      <section className="page-hero">
+        <div className="container">
+          <h2>404 Not Found</h2>
+
+          <p>お探しのページは見つかりませんでした。</p>
+        </div>
+      </section>
+
+      <section className="section">
+      </section>
+    </>
+  );
+}
