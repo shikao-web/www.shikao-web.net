79ca92662131d5b78850b178e141ab48ec53a26d
shikao-web <sikao1st30@gmail.com>
2026-08-03 14:07:38 +0900

    changed profile icon

--- app/page.tsx
+++ app/page.tsx
@@ -57,7 +57,7 @@ export default async function HomePage() {
                     post.thumbnail ? (
                       <img src={post.thumbnail.url} alt={post.title} />
                     ) : (
-                      <img src="/img/SHIKAO-150x150.jpg" alt="news thumbnail" />
+                      <img src="/img/SHIKAO-150x150.png" alt="news thumbnail" />
                     )
                   }
                   <div className="card__body">
